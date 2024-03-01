86380d7ade1af337b481e123b279bf35c1807257
SYED SAAD ALI <76819782+syed-saad-ali-046@users.noreply.github.com>
2024-03-01 22:11:28 +0500

    Update database.js

--- Server/Config/database.js
+++ Server/Config/database.js
@@ -1,7 +1,7 @@
 // server/config/database.js
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Farhang:Farhang@farhang1.o060rhl.mongodb.net/User', {
+mongoose.connect('mongodb+srv://Farhang:Farhang@farhang1.o060rhl.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
