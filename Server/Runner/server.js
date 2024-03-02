28e6aa0df824956bc166e158144ed1c512c04030
SYED SAAD ALI <76819782+syed-saad-ali-046@users.noreply.github.com>
2024-03-03 01:26:04 +0500

    Update server.js

--- Server/Runner/server.js
+++ Server/Runner/server.js
@@ -62,3 +62,19 @@ app.listen(port, () => {
 app.all('/', (req, res) => {
   res.send('Result: [OK].');
 });
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Hello Page</title>
+    </head>
+    <body>
+      <h1>Hello</h1>
+    </body>
+    </html>
+  `);
+});
